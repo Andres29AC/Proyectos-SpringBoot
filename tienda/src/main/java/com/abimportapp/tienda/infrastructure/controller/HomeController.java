d2d9714c9a3b5f3daae81e832b43a31e889efc2a
Andres Amaro <lionAS_45@hotmail.com>
2024-08-20 17:01:34 -0500

    vista detalle de producto agregada

--- tienda/src/main/java/com/abimportapp/tienda/infrastructure/controller/HomeController.java
+++ tienda/src/main/java/com/abimportapp/tienda/infrastructure/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.abimportapp.tienda.infrastructure.controller;
 
+import com.abimportapp.tienda.application.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    private final ProductService productService;
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("products", productService.getProducts());
         return "home";
+    }
+    @GetMapping("/product-detail/{id}")
+    public String productDetail(){
+        return "user/productdetail";
     }
 }
